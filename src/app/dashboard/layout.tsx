ecd64294959abb55ab6f8c471eb8eb50d1b9d531
Joel Friedrich <joelfriedrichdev@gmail.com>
2025-09-18 20:40:54 +0000

    I need a hide menu button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,6 +19,7 @@ export default function DashboardLayout({
       <Sidebar>
         <SidebarHeader>
           <Logo />
+          <SidebarTrigger className="hidden md:flex" />
         </SidebarHeader>
         <SidebarContent>
           <DashboardNav />
