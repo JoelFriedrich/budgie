9f0a13d0635afe6ee44928544315402cd5eeda06
Joel Friedrich <joelfriedrichdev@gmail.com>
2025-09-18 18:56:35 +0000

    actually lets just make icon.png the main image since we're using that a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <div className="flex items-center gap-2 text-lg font-bold text-sidebar-foreground">
-      <Image src="/budgie-logo.png" alt="Budgie Logo" width={28} height={28} />
+      <Image src="/icon.png" alt="Budgie Logo" width={28} height={28} />
       <span>Budgie</span>
     </div>
   );
